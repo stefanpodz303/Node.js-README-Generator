579b09bfe2fd7d4fff15005f8459a4bc7761af69
The Podz <stefanpodz@yahoo.com>
2021-03-26 18:54:49 -0400

    added index.js code to prompt user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,101 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// // TODO: Create an array of questions for user input
+// const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = require('./utils/generateMarkdown');
+
+// res => `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+//         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+// </head>
+// <body>
+//     <div class="jumbotron jumbotron-fluid">
+//         <div class="container">
+//             <h1 class="display-4">Portfolio</h1>
+//             <p class="lead">Check out my Portfolio.</p>
+//         </div>
+//         <div class=display-2 id='name'>${res.name}</div>
+//         <div class=display-2 id='location'>${res.location}</div>
+//         <div class=display-2 id='bio'>${res.bio}</div>
+//         <div class=display-2><a href='#' id='linkedin'>${res.linkedin}</a></div>
+//         <div class=display-2><a href='#' id='github'>${res.github}</a></div>
+//     </div>
+// <script src =index.js type=text/javascript></script>
+// </body>
+// </html>`
+
+inquirer
+  .prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the project usage."
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Please provide the project licence or your badge link."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contributing parties."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide the project tests."
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repository link?"
+    },
+  ])
+
+  .then((data) => {
+
+    fs.writeFile("README.md", generateREADME(data), (err) =>
+      err ? console.log(err) : console.log('New README file created!')
+    );
+  });
