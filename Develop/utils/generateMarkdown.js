8713a54eaf45b32f88a056ad9fc84663385659b5
The Podz <stefanpodz@yahoo.com>
2021-03-27 09:55:50 -0400

    updated generateMarkdown code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,46 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateMarkdown = data => 
 
-`;
-}
+`# ${data.title}
+${data.badge}
+
+## Table of contents
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+
+## Description 
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${data.license}
+
+## Contributors
+${data.contributors}
+
+## Test
+${data.test}
+
+## Repository
+- ${data.repo})
+
+## GitHub
+- ${data.githubInfo}
+- ${data.email}`;
+
 
 module.exports = generateMarkdown;
